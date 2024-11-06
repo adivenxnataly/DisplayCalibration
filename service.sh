f57a63cf6ee5552da77ced2d77c6e2f06cafdfd3
adivenxnataly <adinata.ch@gmail.com>
2024-11-06 23:15:34 +0700

    calling surfaceflinger service & property

--- service.sh
+++ service.sh
@@ -3,10 +3,6 @@
 # Persistent Properties
 
 resetprop -n persist.sys.sf.color_saturation 1.25
-resetprop -n persist.sys.sf.native_mode 0
-
-# Add blur supported for MIUI
-resetprop -n ro.config.low_ram.threshold_gb 2
 
 start panel_color
 
@@ -20,5 +16,4 @@ done
 # Sleep an additional 40s to ensure init is finished
 sleep 40
 
-service call SurfaceFlinger 1023 i32 0
 service call SurfaceFlinger 1022 f 1.25
